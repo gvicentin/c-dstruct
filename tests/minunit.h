8afd5a15f0a5eb93069597be05ab35fed7fb0751
Guilherme Vicentin <gvicentin@ciandt.com>
2022-12-03 19:43:48 -0300

    Testing creating and expand

--- tests/minunit.h
+++ tests/minunit.h
@@ -2,8 +2,8 @@
 #define MINUNIT_H
 
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); g_testsRun++; \
+#define mu_run_test(test) do { char *message = test(); g_tests_run++; \
                                 if (message) return message; } while (0)
-extern int g_testsRun;
+extern int g_tests_run;
 
 # endif // MINUNIT_H
--- tests/test_list.c
+++ tests/test_list.c
@@ -1,65 +1,90 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "minunit.h"
 #include "list.c"
 
-#define MSG_LEN 512
+#define FAIL_MSG_LEN 512
 
-int g_testsRun = 0;
+int g_tests_run = 0;
 
-static char m_errorMsg[MSG_LEN];
-static const char *m_input[] = {
-    "192.168.15.134/24",
-    "172.16.65.23/20",
-    "10.169.23.212/23",
-    "81.215.25.74/18",
-    "127.0.75.88/8"
-};
+static char m_fail_msg[FAIL_MSG_LEN];
 
-static char * testGetBroadcast(void);
-static char * allTests(void);
+static char * test_list_create(void);
+static char * test_list_expand(void);
+static char * all_tests(void);
 
 int main(int argc, char **argv) {
-    char *result = allTests();
-    if (result != 0) {
+    char *result = all_tests();
+
+    if (result != NULL) {
         printf("%s\n", result);
     }
     else {
         printf("ALL TESTS PASSED\n");
     }
-    printf("Tests run: %d\n", g_testsRun);
+    printf("Tests run: %d\n", g_tests_run);
 
-    return result != 0;
+    return result != NULL;
 }
 
-static char * testGetBroadcast(void) {
-    // Cidr cidr;
-    // uint32_t broadcast;
-    // uint32_t expectedBroadcasts[] = {
-    //     0xC0A80FFF,
-    //     0xAC104FFF,
-    //     0x0AA917FF,
-    //     0x51D73FFF,
-    //     0x7FFFFFFF
-    // };
+static char * test_list_create(void) {
+    list tlist;
 
-    // for (int i = 0; i < 5; ++i) {
-    //     CidrCreate(&cidr, m_input[i]);
-    //     broadcast = CidrGetBroadcast(&cidr);
-    //     snprintf(
-    //         m_errorMsg,
-    //         MSG_LEN,
-    //         "CidrGetBroadcast(%s), Expect: %X, Got %X",
-    //         m_input[i], expectedBroadcasts[i], broadcast
-    //     );
-    //     mu_assert(m_errorMsg, broadcast == expectedBroadcasts[i]);
-    // }
-    return 0;
+    list_create(&tlist);
+
+    // size
+    snprintf(m_fail_msg, FAIL_MSG_LEN,
+             "[list_create] Expected size (%u), got (%zu)", 0, tlist.size);
+    mu_assert(m_fail_msg, tlist.size == 0);
+
+    // capacity
+    snprintf(m_fail_msg, FAIL_MSG_LEN,
+             "[list_create] Expected capacity (%u), got (%zu)", INITIAL_CAPACITY, tlist.capacity);
+    mu_assert(m_fail_msg, tlist.capacity == INITIAL_CAPACITY);
+
+    // data array
+    mu_assert("[list_create] Array buffer can't be NULL",
+              tlist.data_array != NULL);
+
+    list_destroy(&tlist);
+
+    return NULL;
 }
 
-static char * allTests() {
-    mu_run_test(testGetBroadcast);
-    return 0;
+static char * test_list_expand(void) {
+    list tlist;
+    size_t new_capacity = 100, prev_capacity;
+
+    list_create(&tlist);
+    list_expand(&tlist, new_capacity);
+
+    // test expand capacity
+    snprintf(m_fail_msg, FAIL_MSG_LEN,
+             "[list_expand] Expected capacity (%zu), got (%zu)",
+             new_capacity, tlist.capacity);
+    mu_assert(m_fail_msg, tlist.capacity == new_capacity);
+
+    // test when not to expand
+    prev_capacity = new_capacity;
+    new_capacity = 50;
+    list_expand(&tlist, new_capacity);
+    snprintf(m_fail_msg, FAIL_MSG_LEN,
+             "[list_expand] Expected capacity (%zu), got (%zu)",
+             prev_capacity, tlist.capacity);
+    mu_assert(m_fail_msg, tlist.capacity == prev_capacity);
+
+    list_destroy(&tlist);
+
+    return NULL;
+}
+
+static char * all_tests() {
+    // run all tests
+    mu_run_test(test_list_create);
+    mu_run_test(test_list_expand);
+
+    return NULL;
 }
